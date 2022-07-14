fb9b2e3761a95acf8694456f3f55267695d3a807
holmesramirez <holmesramirez@hotmail.com>
2022-07-14 15:39:31 -0500

    Se agrega el proyecto agenda contactos

---
+++ index.php
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Agenda de contactos</title>
+    <link rel="icon" type="image/png" href="public/img/One_Piece_Anime_Logo.png">
+    <?php require_once "dependencias.php"; ?>
+</head>
+<body>
+    
+</body>
+</html>
